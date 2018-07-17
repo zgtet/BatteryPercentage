d579594b082cecd1581b196ea8b875c0266a8fd9
Ng <CON004@zebra.com>
2018-07-17 16:06:19 +0800

    Initial commit

---
+++ app/src/main/java/com/symbol/batterypercentage/batterypercentage/MainActivity.java
@@ -0,0 +1,31 @@
+package com.symbol.batterypercentage.batterypercentage;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView batteryTxt;
+    private BroadcastReceiver mBatInfoReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context ctxt, Intent intent) {
+            int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+            batteryTxt.setText("Battery Level: " + String.valueOf(level) + "%");
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.activity_main);
+        batteryTxt = (TextView) this.findViewById(R.id.batteryTxt);
+        this.registerReceiver(this.mBatInfoReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    }
+}
